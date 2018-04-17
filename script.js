cfc5a7e007293693ea2985eee28a5e3e84249d93
Arl33t <arl33t@gmail.com>
2018-04-17 12:49:07 +0200

    mastah
    how do i github?

---
+++ script.js
@@ -0,0 +1,15 @@
+window.onload=function(){
+	document.querySelector(".myBtn").addEventListener("click", myFunction);
+}
+let state = 0;
+function myFunction() {
+    if (state === 0) {
+		document.querySelector(".modul").style.height ="800px";
+		document.querySelector(".modul").style.margin ="0px 0px 0px 100px";
+		state++;
+    }else {
+    	document.querySelector(".modul").style.height ="300px";
+    	document.querySelector(".modul").style.margin ="5px";
+    	state--;
+    }
+}
