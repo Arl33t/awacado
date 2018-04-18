2270a674864c11886dcd340f3993ac3157add490
Arl33t <arl33t@gmail.com>
2018-04-18 10:34:03 +0200

    scriptstuff
    stuff in script

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-window.onload=function(){
-	document.querySelector(".myBtn").addEventListener("click", myFunction);
-}
+var el = document.querySelectorAll(".myBtn");
 let state = 0;
-function myFunction() {
-    if (state === 0) {
-		document.querySelector(".modul").style.height ="800px";
-		document.querySelector(".modul").style.margin ="0px 0px 0px 100px";
-		state++;
+for (var i = 0; i < el.length; i++) {
+    el[i].addEventListener('click', function() {
+        if (state === 0) {
+        document.querySelector(".modul").style.height ="800px";
+        document.querySelector(".modul").style.margin ="0px 0px 0px 100px";
+        state++;
     }else {
-    	document.querySelector(".modul").style.height ="300px";
-    	document.querySelector(".modul").style.margin ="5px";
-    	state--;
+        document.querySelector(".modul").style.height ="300px";
+        document.querySelector(".modul").style.margin ="5px";
+        state--;
     }
+    });
 }
+
+/*function myFunction() {
+    if (state === 0) {
+        document.querySelector(".modul").style.height ="800px";
+        document.querySelector(".modul").style.margin ="0px 0px 0px 100px";
+        state++;
+    }else {
+        document.querySelector(".modul").style.height ="300px";
+        document.querySelector(".modul").style.margin ="5px";
+        state--;
+    }
+}*/
